a0c305bfb98249b043e25445664eff4e0e213742
Azra Animating <42267730+AzraAnimating@users.noreply.github.com>
2019-11-29 15:40:37 +0100

    Create Waggon.java

---
+++ Waggon.java
@@ -0,0 +1,17 @@
+
+/**
+ * @author 
+ * @version 
+ */
+public class Waggon
+{
+    int number = 0;
+    public Waggon(int pNumber)
+    {
+        number = pNumber;
+    }
+    public int getNumber()
+    {
+        return number;
+    }
+}
